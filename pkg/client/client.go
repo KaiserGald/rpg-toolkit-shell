ba2a735b1d8cb08bef572bd90cc3b82145a9cb3a
Scott Isenberg <galdwulfram@gmail.com>
2018-01-05 17:47:59 -0600

    adds TCP connection detection and adds graceful shutdown

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -3,23 +3,41 @@ package client
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
+	"os"
 	"strings"
+	"time"
 )
 
 var servAddr string
 
 // Start starts the tcp connection
-func Start() *net.TCPConn {
+func Start() (*net.TCPConn, error) {
 	servAddr = "127.0.0.1:8081"
 	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+	handleError("ResolveTCPAddr failed: ", err)
+
+	c, err := net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
-		fmt.Println("ResolveTCPAddr failed: ", err)
+		fmt.Printf("   Error creating connection to server: %v\n", err)
+		fmt.Println("   Attempt to reconnect? y/n ")
+		r := bufio.NewReader(os.Stdin)
+		i := getInput(r)
+		if i == "y" || i == "yes" {
+			for err != nil {
+				fmt.Println("   Attempting to reconnect in 10 seconds...")
+				time.Sleep(10 * time.Second)
+				fmt.Println("   Reconnecting now...")
+				c, err = net.DialTCP("tcp", nil, tcpAddr)
+				if err != nil {
+					fmt.Printf("   Error creating connection to server: %v\n", err)
+				}
+			}
+		}
 	}
 
-	c, _ := net.DialTCP("tcp", nil, tcpAddr)
-
-	return c
+	return c, nil
 }
 
 // Send sends a message to the connection
@@ -28,10 +46,28 @@ func Send(s string, c *net.TCPConn) {
 }
 
 // Read reads a message from the connection
-func Read(c *net.TCPConn) {
-	message, _ := bufio.NewReader(c).ReadString('\n')
+func Read(c *net.TCPConn) (string, error) {
+	message, err := bufio.NewReader(c).ReadString('\n')
+	if err != nil {
+		return "", err
+	}
 	if message != "" {
 		message = strings.Trim(message, "\n")
-		fmt.Println("-> " + message)
+		return message, nil
+	}
+
+	return message, nil
+}
+
+func getInput(r *bufio.Reader) string {
+	t, err := r.ReadString('\n')
+	handleError("Error reading from console: ", err)
+	t = strings.Trim(t, "\n")
+	return t
+}
+
+func handleError(m string, e error) {
+	if e != nil {
+		log.Println(m+"%v\n", e)
 	}
 }
--- pkg/rpgAppSh/rpgAppSh.go
+++ pkg/rpgAppSh/rpgAppSh.go
@@ -3,10 +3,13 @@ package rpgAppSh
 import (
 	"bufio"
 	"fmt"
+	"log"
+	"net"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/KaiserGald/rpgAppSh/pkg/client"
 )
@@ -21,8 +24,14 @@ const (
 	stop string = "stop"
 )
 
+var p *os.Process
+
 // Start starts the console
 func Start() error {
+
+	var err error
+	p, err = os.FindProcess(os.Getpid())
+	handleError("Error finding pid: ", err)
 	go run()
 	waitForSignal()
 	return nil
@@ -30,40 +39,87 @@ func Start() error {
 
 // run starts the loop for the console
 func run() {
-	conn := client.Start()
+	conn, err := client.Start()
+	handleError("Error starting client: ", err)
 	r := bufio.NewReader(os.Stdin)
 	var text string
 
+	fmt.Println("\n")
 	fmt.Println("Welcome to Unlicht RPG Toolkit Shell")
 	fmt.Println(strings.Repeat("-", 100))
 	for {
+		client.Send("\n", conn)
+		_, err := client.Read(conn)
+		if err != nil {
+			reconnect()
+		}
 		fmt.Print(":> ")
 		text = getInput(r)
 
+		// check for exit first
 		if text == exit {
-			fmt.Print("Are you sure you want to exit? y/n :> ")
+			fmt.Print("   Are you sure you want to exit? y/n ")
 			text = getInput(r)
 			if text == y || text == yes {
 				fmt.Println("Goodbye!")
-				os.Exit(0)
+				err := p.Signal(os.Interrupt)
+				handleError("   Error emitting the interrupt signal: ", err)
 			}
+			// else send and receive response
 		} else {
 			client.Send(text, conn)
-			client.Read(conn)
+			res, err := client.Read(conn)
+			if err != nil {
+				reconnect()
+			}
+
+			handleResponse(res)
 		}
 	}
 }
 
+func reconnect() *net.TCPConn {
+	fmt.Println("   Lost connection to client, attempting reconnect.")
+	time.Sleep(5 * time.Second)
+	conn, err := client.Start()
+	handleError("Error starting client: ", err)
+	return conn
+}
+
+// getInput reads input from the console
 func getInput(r *bufio.Reader) string {
-	t, _ := r.ReadString('\n')
+	t, err := r.ReadString('\n')
+	handleError("Error reading from console: ", err)
 	t = strings.Trim(t, "\n")
 	return t
 }
 
+// handleResponse takes the comand string and routes it to the proper use case
+func handleResponse(res string) {
+	switch res {
+	case stop:
+		fmt.Printf("   Stop command received, shutting server down...\n")
+		fmt.Println("Goodbye!")
+		err := p.Signal(os.Interrupt)
+		handleError("   Error emitting the interrupt signal: ", err)
+
+	default:
+		fmt.Printf("   Unknown command received.\n")
+	}
+}
+
+// waitForSignal waits for an interrupt or terminate signal before handling shut down of the program.
 func waitForSignal() {
 	ch := make(chan os.Signal)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	s := <-ch
 
-	fmt.Println("Got signal: %v, exiting.\n", s)
+	fmt.Printf("Got signal: %v, exiting.\n", s)
+	time.Sleep(2 * time.Second)
+}
+
+func handleError(m string, e error) {
+	if e != nil {
+		log.Println(m+"%v\n", e)
+	}
 }
